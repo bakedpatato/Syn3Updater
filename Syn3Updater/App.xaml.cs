e70665a1a4b082fbd4139d0e7d0ae54c01e57093
Chris Choi <hscchoi@gmail.com>
2021-04-05 21:41:01 -0700

    wiring stuff up

--- Syn3Updater/App.xaml.cs
+++ Syn3Updater/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
 using System.Windows.Threading;
 
 namespace Cyanlabs.Syn3Updater
@@ -6,18 +10,43 @@ namespace Cyanlabs.Syn3Updater
     /// <summary>
     ///     Interaction logic for App.xaml
     /// </summary>
-    public partial class App
+    public partial class App : Application
     {
+        private readonly IHost host;
+
+        public static IServiceProvider ServiceProvider { get; private set; }
         #region Methods
 
-        protected override void OnStartup(StartupEventArgs e)
+        protected override async void OnStartup(StartupEventArgs e)
         {
+            await host.StartAsync();
             base.OnStartup(e);
             {
                 DispatcherUnhandledException += App_DispatcherUnhandledException;
             }
-
             ApplicationManager.Instance.Initialize();
+
+        }
+
+        public App()
+        {
+            host = Host.CreateDefaultBuilder()
+          .ConfigureServices((context, services) =>
+          {
+              ConfigureServices(context.Configuration, services);
+          }).ConfigureLogging(logging => { })
+          .Build();
+
+            ServiceProvider = host.Services;
+        }
+
+        private void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+        {
+
+            // Register all ViewModels.
+            services.AddSingleton<UI.Tabs.HomeViewModel>();
+            // Register all the Windows of the applications.
+            services.AddTransient<UI.MainWindow>();
         }
 
         private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
@@ -26,8 +55,12 @@ namespace Cyanlabs.Syn3Updater
             e.Handled = true;
         }
 
-        private void App_OnExit(object sender, ExitEventArgs e)
+        private async void App_OnExit(object sender, ExitEventArgs e)
         {
+            using (host)
+            {
+                await host.StopAsync(System.TimeSpan.FromSeconds(5));
+            }
             //  throw new NotImplementedException();
         }
 
